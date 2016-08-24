a30b84a07dd2fa8096aaca3ae21511ad97fea5e3
wbhniwanl <8825698107@163.com>
2016-08-24 14:31:25 +0800

    xiugaiwancheng

--- expression/testExpression.php
+++ expression/testExpression.php
@@ -6,8 +6,8 @@
  * Time: 10:46
  */
 $test = new \Ary\Workspece\Expression();
-$arr  = array("Linux RedHat9.0", "Apache2.2.9", "MySQL5.0.51", "PHP5.2.6", "LAMP", "100");
-var_dump($test->reExpression($arr));
+$arrDate  = array("Linux RedHat9.0", "Apache2.2.9", "MySQL5.0.51", "PHP5.2.6", "LAMP", "100");
+var_dump($test->reExpression($arrDate));
 $row = "http://www.yaochufa.com/index.php";
 echo "<pre>";
 var_dump($test->reExpressionHttp($row));
@@ -15,5 +15,5 @@ echo "<pre>";
 $arr = "<tr><td><a href=\"http://qzone.qq.com\">QQ空间</a></td><td><a href=\"http://www.ganji.com\">赶 集 网</a></td><td>";
 var_dump($test->intercept($arr));
 echo "<pre>";
-$arr = "<tr><td><a href=”http://qzone.qq.com”>QQ空间</a></td><td><a href=\"http://www.ganji.com\">赶 集 网</a></td><td>";
-var_dump($test->delectHtml($arr));
+$arrDelect = "<tr><td><a href=”http://qzone.qq.com”>QQ空间</a></td><td><a href=\"http://www.ganji.com\">赶 集 网</a></td><td>";
+var_dump($test->delectHtml($arrDelect));
